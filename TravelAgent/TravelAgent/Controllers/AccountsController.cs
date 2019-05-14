78fc21755be69cb5ab9a446d9aafa91b2450ce9f
Paulius <paulius.grigaliunas.pg@gmail.com>
2019-05-14 11:33:18 +0300

    Create AccountController Registration method, add RegisterAccountRequest

---
+++ TravelAgent/TravelAgent/Controllers/AccountsController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelAgent.ClientApp
+{
+    public class AccountsController
+    {
+    }
+}
