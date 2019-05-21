cc342d2be7725fe2628c2e09753a909dd9fbca15
Paulius <paulius.grigaliunas.pg@gmail.com>
2019-05-21 11:20:16 +0300

    add migration for expand Travel Entity

---
+++ TravelAgent/TravelAgent/Migrations/20190521081637_expandTravel.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TravelAgent.Migrations
+{
+    public partial class expandTravel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TravelId",
+                table: "Transports",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transports_TravelId",
+                table: "Transports",
+                column: "TravelId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transports_Travels_TravelId",
+                table: "Transports",
+                column: "TravelId",
+                principalTable: "Travels",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transports_Travels_TravelId",
+                table: "Transports");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Transports_TravelId",
+                table: "Transports");
+
+            migrationBuilder.DropColumn(
+                name: "TravelId",
+                table: "Transports");
+        }
+    }
+}
