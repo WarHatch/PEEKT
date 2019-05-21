dd08c8fe410519210552298bd0fdfba40d2dd2fb
Paulius <paulius.grigaliunas.pg@gmail.com>
2019-05-21 11:31:24 +0300

    changed Transport prop type TypeOfTransport to string

--- TravelAgent/TravelAgent/Data/Entities/Transport.cs
+++ TravelAgent/TravelAgent/Data/Entities/Transport.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
-using TravelAgent.DataContract.Enums;
 
 namespace TravelAgent.Data.Entities
 {
@@ -11,6 +10,6 @@ namespace TravelAgent.Data.Entities
     {
         public int Id { get; set; }
         public string Description { get; set; }
-        public Vehicle TypeOfTransport { get; set; }
+        public string TypeOfTransport { get; set; }
     }
 }
