0bfb9d35a98969e2bfcf02a84a00e39b3d27dd29
Tomas <Hi>
2019-05-26 22:50:52 +0300

    Solved CORS issue

--- TravelAgent/TravelAgent/Controllers/EmployeeTravelController.cs
+++ TravelAgent/TravelAgent/Controllers/EmployeeTravelController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
 using TravelAgent.Data.Entities;
 using TravelAgent.Data.Repositories.Interfaces;
 using TravelAgent.DataContract.Requests;
@@ -10,6 +11,7 @@ using TravelAgent.DataContract.Requests;
 namespace TravelAgent.Controllers
 {
     [ApiController]
+    [EnableCors("AllowOrigin")]
     [Route("api/[controller]")]
     public class EmployeeTravelController : ControllerBase
     {
--- TravelAgent/TravelAgent/Controllers/OfficesController.cs
+++ TravelAgent/TravelAgent/Controllers/OfficesController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
 using TravelAgent.Data.Entities;
 using TravelAgent.Data.Repositories.Interfaces;
 using TravelAgent.DataContract.Requests;
@@ -11,6 +12,7 @@ using TravelAgent.DataContract.Requests;
 namespace TravelAgent.Controllers
 {
     [ApiController]
+    [EnableCors("AllowOrigin")]
     [Route("api/[controller]")]
     public class OfficesController : ControllerBase
     {
